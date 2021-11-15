0ee117c6c64cdf061bb1517a999a3587bd0c435e
Filippo <filippo99magi@gmail.com>
2021-11-15 11:12:59 +0100

    test

--- src/LastLevelOpt/Graph.cs
+++ src/LastLevelOpt/Graph.cs
@@ -5,7 +5,7 @@ namespace LastLevelOpt
 {
     public class Graph
     {
-        public List<HashSet<Node>> labeledNode {get; private set;}
+        public List<HashSet<Node>> labeledNode {get; private set;} 
         public HashSet<Node> invalidNode {get; private set;}
 
         //TODO capire se prima iterazione vanno inseriti in labeledNode o in invalidNode
@@ -13,31 +13,32 @@ namespace LastLevelOpt
         public Graph()
         {
             this.labeledNode = new List<HashSet<Node>>();
+            this.labeledNode.Add(new HashSet<Node>());
         }
 
         public Graph(Node node)
         {
             this.labeledNode = new List<HashSet<Node>>();
-            labeledNode[0].Add(node);
+            this.labeledNode.Add(new HashSet<Node>(new Node[]{node}));
         }
         public Graph(params Node[] nodes)
         {
             this.labeledNode = new List<HashSet<Node>>();
             foreach (var n in nodes)
-                labeledNode[0].Add(n);//TODO capire se c'è modo più veloce
+                this.labeledNode[0].Add(n);//TODO capire se c'è modo più veloce
         }
         public void addNode(Node node)
         {
-            labeledNode[0].Add(node);
+            this.labeledNode[0].Add(node);
         }
         public Node Source => labeledNode.First().SingleOrDefault( x => x is SourceNode);
         //TODO da capire come fare il source, cioè non deve essere una funzione
         public Node Sink {
             get
             {
-                Node sink = invalidNode.SingleOrDefault(x => x is SinkNode);
+                Node sink = this.invalidNode.SingleOrDefault(x => x is SinkNode);
                 if (sink == null)
-                    labeledNode.Last().Single(x => x is SinkNode);
+                    this.labeledNode.Last().Single(x => x is SinkNode);
                 return sink;
             }
         }
