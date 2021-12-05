2a5a10c3aeca55f71e50e5608fe3e308ee1e68e1
Filippo <filippo99magi@gmail.com>
2021-12-05 16:06:15 +0100

    added test5

--- src/BFS.Tests/SickPropagationGraphOptTests.cs
+++ src/BFS.Tests/SickPropagationGraphOptTests.cs
@@ -139,8 +139,52 @@ public class SickPropagationGraphOptTests
         Graph graph = new Graph(s, n1, n2, n3, n4, n5, n6, n7, n8, n9, t);
         var res = BfsSickPropagationGraphOpt.FlowFordFulkerson(graph);
         Assert.Equal(60, res);
+    }
 
+    [Fact]
+    public void Test5()
+    {
+        SinkNode t = new SinkNode("t");
+        Node n17 = new Node("17");
+        Node n16 = new Node("16");
+        Node n15 = new Node("15");
+        Node n14 = new Node("14");
+        Node n13 = new Node("13");
+        Node n12 = new Node("12");
+        Node n11 = new Node("11");
+        Node n10 = new Node("10");
+        Node n9 = new Node("9");
+        Node n8 = new Node("8");
+        Node n7 = new Node("7");
+        Node n6 = new Node("6");
+        Node n5 = new Node("5");
+        Node n4 = new Node("4");
+        Node n3 = new Node("3");
+        Node n2 = new Node("2");
+        Node n1 = new Node("1");
+        SourceNode s = new SourceNode("s");
+        s.AddEdge((n1, 20), (n2, 30), (n3, 30));
+        n1.AddEdge((n5, 5), (n6, 10));
+        n2.AddEdge((n1, 5), (n3, 5), (n5, 15));
+        n3.AddEdge((n4, 30), (n5, 10));
+        n4.AddEdge((n7, 25), (n8, 20));
+        n5.AddEdge(n7, 10);
+        n6.AddEdge((n7, 15), (n9, 10));
+        n7.AddEdge((n8, 30), (n10, 10));
+        n8.AddEdge(n10, 15);
+        n9.AddEdge((n7, 5), (n10, 15));
+        n10.AddEdge((n11, 30), (n12, 40), (n13, 25), (n14, 20));
+        n11.AddEdge(n15, 5);
+        n12.AddEdge(n15, 20);
+        n13.AddEdge(n16, 25);
+        n14.AddEdge(n16, 30);
+        n15.AddEdge(t, 50);
+        n16.AddEdge(t, 50);
+        //t.AddEdge(n17, 100);
+        n17.AddEdge(n14, 100);
+        Graph graph = new Graph(s, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14, n15, n16, n17, t);
 
-
+        var res = BfsSickPropagationGraphOpt.FlowFordFulkerson(graph);
+        Assert.Equal(35, res);
     }
 }
